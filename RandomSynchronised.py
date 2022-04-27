79abab5ec7d373f436fb537448e50f1aec438c55
DSHardman <dshardman@outlook.com>
2022-04-27 08:20:00 +0100

    final random data from small

--- RandomSynchronised.py
+++ RandomSynchronised.py
@@ -48,7 +48,7 @@ with ni.Task() as task:
     task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
     task.write(5)
 
-for i in range(3182, 5000):  # Record 500 probes at each temperature
+for i in range(4333, 5000):  # Record 500 probes at each temperature
 
     # Random xy positions & depth
     x = random.random()*xupperbound
